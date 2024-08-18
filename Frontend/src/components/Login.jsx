31f9840abe2e17ca8dfa6dc75823bd8125556205
rushabh782 <155352920+rushabh782@users.noreply.github.com>
2024-08-18 15:23:46 +0530

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -18,7 +18,7 @@ function Login() {
       password: data.password,
     };
     await axios
-      .post("http://localhost:4001/user/login", userInfo)
+      .post("https://bookstoreapp-backend-relz.onrender.com", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
